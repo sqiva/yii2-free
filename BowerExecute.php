fceaba985470fca66867478a5ca5ab104d541672
raymondpesik <raymond_robertus@yahoo.com>
2017-03-17 21:33:03 +0700

    Update BowerExecute.php

--- BowerExecute.php
+++ BowerExecute.php
@@ -1,3 +1,3 @@
 <?php
 //~ create bower install
-exec("\"C:\Program Files\Git\bin\sh.exe\" --login -i -c \"bower install\"");
+exec('"C:\Program Files\Git\bin\sh.exe" --login -i -c "bower install"');
